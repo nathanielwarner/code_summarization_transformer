8b0f694ff955504d223b6325e3e56a29954dfbe1
Nathaniel Warner <nathanielwarner77@gmail.com>
2020-06-19 18:25:27 -0700

    Add some options for evaluation

--- evaluate_summarization.py
+++ evaluate_summarization.py
@@ -11,15 +11,20 @@ nltk.download('wordnet')
 parser = argparse.ArgumentParser(description="Evaluate a model's source code summarization")
 
 parser.add_argument("--model_path", help="Path to the model", required=True)
+parser.add_argument("--eval_set_path", help="Path to the set to evaluate on", required=True)
 parser.add_argument("--batch_size", help="Size of batches to feed into the model", type=int, default=64)
+parser.add_argument("--print_out", help="Print correct and predicted translation, and METEOR score for every sentence",
+                    type=bool, default=False)
 args = vars(parser.parse_args())
 
 model_path = args["model_path"]
+eval_set_path = args["eval_set_path"]
 batch_size = args["batch_size"]
+print_out = args["print_out"]
 
 transformer = Transformer(model_path)
 
-dataset = tdu.load_json_dataset("data/leclair_java/test.json")
+dataset = tdu.load_json_dataset(eval_set_path)
 len_dataset = len(dataset)
 codes = [ex[1] for ex in dataset]
 true_summaries = [[ex[0]] for ex in dataset]
@@ -33,14 +38,16 @@ for batch_num in tqdm.trange(num_batches):
     predicts_batch = transformer.translate_batch(codes_batch, preprocessed=True)
     predicts.extend(predicts_batch)
     for i in range(batch_num * batch_size, batch_num * batch_size + size_of_batch):
-        print("%d of %d" % (i, len_dataset))
-        print("Code: %s" % codes[i])
-        print("True Summaries: %s" % true_summaries[i])
-        print("Predicted Summary: %s" % predicts[i])
+        if print_out:
+            print("%d of %d" % (i, len_dataset))
+            print("Code: %s" % codes[i])
+            print("True Summaries: %s" % true_summaries[i])
+            print("Predicted Summary: %s" % predicts[i])
         meteor = nltk.translate.meteor_score.meteor_score(true_summaries[i], predicts[i])
-        print("METEOR score: %.4f" % meteor)
+        if print_out:
+            print("METEOR score: %.4f" % meteor)
+            print()
         meteors.append(meteor)
-        print()
 
 average_meteor = np.mean(meteors)
 print("Average METEOR score: %.4f\n" % average_meteor)
