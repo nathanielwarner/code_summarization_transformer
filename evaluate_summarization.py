b3fd176401d30aebb7402880626105ac9d47630a
Nathaniel Warner <nathanielwarner77@gmail.com>
2020-07-01 12:20:25 -0700

    Update evaluate_summarization for compatibility

--- evaluate_summarization.py
+++ evaluate_summarization.py
@@ -1,8 +1,9 @@
+import tensorflow as tf
 import nltk
 import numpy as np
-import text_data_utils as tdu
 import argparse
 import tqdm
+import os
 from transformer import Transformer
 
 
@@ -10,49 +11,64 @@ nltk.download('wordnet')
 
 parser = argparse.ArgumentParser(description="Evaluate a model's source code summarization")
 
-parser.add_argument("--model_path", help="Path to the model", required=True)
-parser.add_argument("--eval_set_path", help="Path to the set to evaluate on", required=True)
+parser.add_argument("--model_path", help="Path to the model", required=True, type=str)
+parser.add_argument("--dataset_path", help="Path to the dataset, which contains the eval set as well the tokenizers", required=True, type=str)
+parser.add_argument("--eval_set_filename_prefix", help="Prefix of the filename of the dataset to evaluate on", required=True, type=str)
+parser.add_argument("--beam_width", help="Beam width for beam search decoding", type=int, default=10)
 parser.add_argument("--batch_size", help="Size of batches to feed into the model", type=int, default=64)
 parser.add_argument("--print_out", help="Print correct and predicted translation, and METEOR score for every sentence",
                     type=bool, default=False)
 args = vars(parser.parse_args())
 
 model_path = args["model_path"]
-eval_set_path = args["eval_set_path"]
+dataset_path = os.path.abspath(args["dataset_path"])
+eval_set_prefix = args["eval_set_filename_prefix"]
+eval_codes_file = os.path.join(dataset_path, eval_set_prefix + "_codes.txt")
+eval_nl_file = os.path.join(dataset_path, eval_set_prefix + "_nl.txt")
+beam_width = args["beam_width"]
 batch_size = args["batch_size"]
 print_out = args["print_out"]
 
-transformer = Transformer(model_path)
+code_spm_path = os.path.join(dataset_path, "code_spm.model")
+nl_spm_path = os.path.join(dataset_path, "nl_spm.model")
 
-dataset = tdu.load_json_dataset(eval_set_path)
-len_dataset = len(dataset)
-codes = [ex[0] for ex in dataset]
-true_summaries = [[ex[1]] for ex in dataset]
+transformer = Transformer(model_path, code_spm_path, nl_spm_path)
 
-num_batches = int((len(codes) / batch_size) + 1)
+codes_set = tf.data.TextLineDataset(eval_codes_file)
+nl_set = tf.data.TextLineDataset(eval_nl_file)
+eval_set = tf.data.Dataset.zip((codes_set, nl_set))
+eval_set = eval_set.batch(batch_size)
+
+trues = []
 predicts = []
 meteors = []
-for batch_num in tqdm.trange(num_batches):
-    codes_batch = codes[batch_num * batch_size: batch_num * batch_size + batch_size]
-    size_of_batch = len(codes_batch)
-    predicts_batch = transformer.translate_batch(codes_batch, preprocessed=True)
-    predicts.extend(predicts_batch)
-    for i in range(batch_num * batch_size, batch_num * batch_size + size_of_batch):
+batch_num = 0
+for codes_batch, nl_batch in eval_set:
+    batch_num += 1
+    if print_out or batch_num % 10 == 0:
+        print("Batch %d" % batch_num)
+    size_of_batch = tf.size(codes_batch).numpy()
+    predicts_batch = transformer.translate_batch(codes_batch, beam_width=beam_width)
+    for i in range(size_of_batch):
+        code = codes_batch[i].numpy().decode('utf-8')
+        nl = nl_batch[i].numpy().decode('utf-8')
+        predict = predicts_batch[i].numpy().decode('utf-8')
+
+        trues.append([nl])
+        predicts.append(predict)
+        
+        meteor = nltk.translate.meteor_score.meteor_score([nl], predict)
         if print_out:
-            print("%d of %d" % (i, len_dataset))
-            print("Code: %s" % codes[i])
-            print("True Summaries: %s" % true_summaries[i])
-            print("Predicted Summary: %s" % predicts[i])
-        meteor = nltk.translate.meteor_score.meteor_score(true_summaries[i], predicts[i])
-        if print_out:
-            print("METEOR score: %.4f" % meteor)
-            print()
+            print("\nCode: %s" % code)
+            print("True Summary: %s" % nl)
+            print("Predicted Summary: %s" % predict)
+            print("METEOR score: %.4f\n" % meteor)
         meteors.append(meteor)
 
 average_meteor = np.mean(meteors)
 print("Average METEOR score: %.4f\n" % average_meteor)
 
-corpus_bleu_4 = nltk.translate.bleu_score.corpus_bleu(true_summaries, predicts, weights=(0.25, 0.25, 0.25, 0.25))
-corpus_bleu_2 = nltk.translate.bleu_score.corpus_bleu(true_summaries, predicts, weights=(0.5, 0.5, 0.0, 0.0))
+corpus_bleu_4 = nltk.translate.bleu_score.corpus_bleu(trues, predicts, weights=(0.25, 0.25, 0.25, 0.25))
+corpus_bleu_2 = nltk.translate.bleu_score.corpus_bleu(trues, predicts, weights=(0.5, 0.5, 0.0, 0.0))
 print("Corpus BLEU-4 score: %.4f" % corpus_bleu_4)
 print("Corpus BLEU-2 score: %.4f" % corpus_bleu_2)
